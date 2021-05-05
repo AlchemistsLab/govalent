708653ece607c8716bd9bd94900830f9ab09da73
zaebee <zaebuntu@gmail.com>
2021-05-05 22:32:28 +0200

    Added govalent client contains all resources available.

---
+++ govalent.go
@@ -0,0 +1,38 @@
+//Package govalent provides the binding for Covalent Rest APIs.
+package govalent
+
+import (
+	"govalent/class_a"
+	"govalent/client"
+	"net/http"
+	"time"
+)
+
+//constant used for API client
+const (
+	APIURL             = "https://api.covalenthq.com/v1/"
+	defaultHTTPTimeout = 80 * time.Second
+)
+
+//Default HTTP client
+var httpClient = &http.Client{Timeout: defaultHTTPTimeout}
+
+// APIKey is Covalent API Key.
+var APIKey string
+
+// ClassA uses endpoint without client.
+func ClassA() *class_a.Client {
+	api := client.New(APIURL, APIKey, httpClient)
+	return &class_a.Client{API: *api}
+}
+
+//Client is the Covalent client. It contains all resources available.
+type Client struct {
+	ClassA class_a.Client
+}
+
+//Init initializes the Binance client with given API key, secret key.
+func (c *Client) Init(apiKey string) {
+	api := client.New(APIURL, apiKey, httpClient)
+	c.ClassA = class_a.Client{API: *api}
+}
