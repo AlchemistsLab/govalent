4988e0d5ccb727dcc19f0c2fba723726c2fcd818
zaebee <zaebuntu@gmail.com>
2021-05-31 12:51:29 +0200

    Cleaned up model names. - Renamed structures for API response. - Updated names for transaction/portfolio/etc...

--- class_a/client.go
+++ class_a/client.go
@@ -12,81 +12,81 @@ type Client struct {
 
 // ClassAInterface describes methods for Class A endpoints.
 type ClassAInterface interface {
-	GetTokenBalances(chainID, address string, params BalanceParams) (PortfolioData, error)
-	GetHistoricalPortfolio(chainID, address string) (PortfolioData, error)
-	GetTransactions(chainID, address string) (TransactionData, error)
-	GetERCTokenTransfers(chainID, address string, params TransferParams) (TransactionData, error)
-	GetBlock(chainID, blockHeight string) (BlockData, error)
-	GetLogEventsByContract(chainID, address string, params LogEventsParams) (LogEventsData, error)
-	GetLogEventsByTopic(chainID, topic string, params LogEventsParams) (LogEventsData, error)
-	GetExternalNFTMetadata(chainID, address, tokenID string) (NFTExternalData, error)
+	GetTokenBalances(chainID, address string, params BalanceParams) (Portfolios, error)
+	GetHistoricalPortfolio(chainID, address string) (Portfolios, error)
+	GetTransactions(chainID, address string) (Transactions, error)
+	GetERCTokenTransfers(chainID, address string, params TransferParams) (Transactions, error)
+	GetBlock(chainID, blockHeight string) (Blocks, error)
+	GetLogEventsByContract(chainID, address string, params LogEventsParams) (LogEvents, error)
+	GetLogEventsByTopic(chainID, topic string, params LogEventsParams) (LogEvents, error)
+	GetExternalNFTMetadata(chainID, address, tokenID string) (NFTMetadata, error)
 }
 
 var _ ClassAInterface = (*Client)(nil)
 
 // GetTokenBalances returns a list of all ERC20 and NFT token balances along with their current spot prices.
-func (c *Client) GetTokenBalances(chainID, address string, params BalanceParams) (PortfolioData, error) {
+func (c *Client) GetTokenBalances(chainID, address string, params BalanceParams) (Portfolios, error) {
 	u := fmt.Sprintf("%v/address/%v/balances_v2/", chainID, address)
-	balance := Balance{}
-	err := c.API.Request("GET", u, params, &balance)
-	return balance.Data, err
+	response := BalanceResponse{}
+	err := c.API.Request("GET", u, params, &response)
+	return response.Data, err
 }
 
 // GetHistoricalPortfolio returns wallet value for the last 30 days at 24 hour timestamps
 // for given chain_id and wallet address.
-func (c *Client) GetHistoricalPortfolio(chainID, address string) (PortfolioData, error) {
+func (c *Client) GetHistoricalPortfolio(chainID, address string) (Portfolios, error) {
 	u := fmt.Sprintf("%v/address/%v/portfolio_v2/", chainID, address)
-	portfolio := PortfolioData{}
-	err := c.API.Request("GET", u, nil, &portfolio)
-	return portfolio, err
+	response := Portfolios{}
+	err := c.API.Request("GET", u, nil, &response)
+	return response, err
 }
 
 // GetTransactions retrieves all transactions for address including their decoded log events.
 //This endpoint does a deep-crawl of the blockchain to retrieve all kinds of transactions that references the address.
-func (c *Client) GetTransactions(chainID, address string) (TransactionData, error) {
+func (c *Client) GetTransactions(chainID, address string) (Transactions, error) {
 	u := fmt.Sprintf("%v/address/%v/transactions_v2/", chainID, address)
-	transaction := Transaction{}
-	err := c.API.Request("GET", u, nil, &transaction)
-	return transaction.Data, err
+	response := TransactionResponse{}
+	err := c.API.Request("GET", u, nil, &response)
+	return response.Data, err
 }
 
 // GetERCTokenTransfers returns ERC20 token transfers. Passing in an ENS resolves automatically.
-func (c *Client) GetERCTokenTransfers(chainID, address string, params TransferParams) (TransactionData, error) {
+func (c *Client) GetERCTokenTransfers(chainID, address string, params TransferParams) (Transactions, error) {
 	u := fmt.Sprintf("%v/address/%v/transfers_v2/", chainID, address)
-	transaction := Transaction{}
-	err := c.API.Request("GET", u, params, &transaction)
-	return transaction.Data, err
+	response := TransactionResponse{}
+	err := c.API.Request("GET", u, params, &response)
+	return response.Data, err
 }
 
 // GetBlock retrieves a single block at block_height.
 // If block_height is set to the value latest, return the latest block available.
-func (c *Client) GetBlock(chainID, blockHeight string) (BlockData, error) {
+func (c *Client) GetBlock(chainID, blockHeight string) (Blocks, error) {
 	u := fmt.Sprintf("%v/block_v2/%v/", chainID, blockHeight)
-	block := Block{}
-	err := c.API.Request("GET", u, nil, &block)
-	return block.Data, err
+	response := BlockResponse{}
+	err := c.API.Request("GET", u, nil, &response)
+	return response.Data, err
 }
 
 // GetLogEventsByContract returns a paginated list of decoded log events emitted by a particular smart contract.
-func (c *Client) GetLogEventsByContract(chainID, address string, params LogEventsParams) (LogEventsData, error) {
+func (c *Client) GetLogEventsByContract(chainID, address string, params LogEventsParams) (LogEvents, error) {
 	u := fmt.Sprintf("%v/events/address/%v/", chainID, address)
-	logEvents := LogEvents{}
-	err := c.API.Request("GET", u, params, &logEvents)
-	return logEvents.Data, err
+	response := LogEventsResponse{}
+	err := c.API.Request("GET", u, params, &response)
+	return response.Data, err
 }
 
 // GetLogEventsByTopic returns a paginated list of decoded log events with one or more topic hashes separated by a comma.
-func (c *Client) GetLogEventsByTopic(chainID, topic string, params LogEventsParams) (LogEventsData, error) {
+func (c *Client) GetLogEventsByTopic(chainID, topic string, params LogEventsParams) (LogEvents, error) {
 	u := fmt.Sprintf("%v/events/topics/%v/", chainID, topic)
-	logEvents := LogEvents{}
-	err := c.API.Request("GET", u, params, &logEvents)
-	return logEvents.Data, err
+	response := LogEventsResponse{}
+	err := c.API.Request("GET", u, params, &response)
+	return response.Data, err
 }
 
 // GetExternalNFTMetadata returns the external metadata for given a NFT contract address and a token ID.
-func (c *Client) GetExternalNFTMetadata(chainID, address, tokenID string) (NFTExternalData, error) {
+func (c *Client) GetExternalNFTMetadata(chainID, address, tokenID string) (NFTMetadata, error) {
 	u := fmt.Sprintf("%v/tokens/%v/nft_metadata/%v/", chainID, address, tokenID)
-	nftMetadata := NFTExternalMetadata{}
-	err := c.API.Request("GET", u, nil, &nftMetadata)
-	return nftMetadata.Data, err
+	response := NFTExternalMetadataResponse{}
+	err := c.API.Request("GET", u, nil, &response)
+	return response.Data, err
 }
--- class_a/model.go
+++ class_a/model.go
@@ -22,7 +22,7 @@ type LogEventsParams struct {
 	PageNumber    int    `json:"page-number"`
 }
 
-// Pagination returns pagination data for each endpoint.
+// Pagination returns pagination metadata for each endpoint.
 type Pagination struct {
 	HasMore    bool `json:"has_more"`
 	PageNumber int  `json:"page_number"`
@@ -30,124 +30,122 @@ type Pagination struct {
 	TotalCount int  `json:"total_count"`
 }
 
-// Balance returns portfolio data for token balances response.
-type Balance struct {
-	Data         PortfolioData `json:"data"`
-	Error        bool          `json:"error"`
-	ErrorMessage string        `json:"error_message"`
-	ErrorCode    int           `json:"error_code"`
+// BalanceResponse returns portfolio data for token balances response.
+type BalanceResponse struct {
+	Data         Portfolios `json:"data"`
+	Error        bool       `json:"error"`
+	ErrorMessage string     `json:"error_message"`
+	ErrorCode    int        `json:"error_code"`
 }
 
-// Transaction returns transaction data.
-type Transaction struct {
-	Data         TransactionData `json:"data"`
-	Error        bool            `json:"error"`
-	ErrorMessage string          `json:"error_message"`
-	ErrorCode    int             `json:"error_code"`
+// TransactionResponse returns transaction data.
+type TransactionResponse struct {
+	Data         Transactions `json:"data"`
+	Error        bool         `json:"error"`
+	ErrorMessage string       `json:"error_message"`
+	ErrorCode    int          `json:"error_code"`
 }
 
-// Block returns block data for given contract address.
-type Block struct {
-	Data         BlockData `json:"data"`
-	Error        bool      `json:"error"`
-	ErrorMessage string    `json:"error_message"`
-	ErrorCode    int       `json:"error_code"`
+// BlockResponse returns block data for given contract address.
+type BlockResponse struct {
+	Data         Blocks `json:"data"`
+	Error        bool   `json:"error"`
+	ErrorMessage string `json:"error_message"`
+	ErrorCode    int    `json:"error_code"`
 }
 
-// NFTExternalMetadata returns.
-type NFTExternalMetadata struct {
-	Data         NFTExternalData `json:"data"`
-	Error        bool            `json:"error"`
-	ErrorMessage string          `json:"error_message"`
-	ErrorCode    int             `json:"error_code"`
+// LogEventsResponse returns decoded log events.
+type LogEventsResponse struct {
+	UpdatedAt    time.Time   `json:"updated_at"`
+	Data         LogEvents   `json:"data"`
+	Error        bool        `json:"error"`
+	ErrorMessage string      `json:"error_message"`
+	ErrorCode    interface{} `json:"error_code"`
 }
 
-type NFTExternalData struct {
-	Items      []PortfolioItem `json:"items"`
-	Pagination Pagination      `json:"pagination"`
+// NFTExternalMetadataResponse returns NFT external metadata for given address and token Id.
+type NFTExternalMetadataResponse struct {
+	Data         NFTMetadata `json:"data"`
+	Error        bool        `json:"error"`
+	ErrorMessage string      `json:"error_message"`
+	ErrorCode    int         `json:"error_code"`
 }
 
-// PortfolioData returns list of items for portfolio endpoint.
-type PortfolioData struct {
-	Address       string          `json:"address"`
-	UpdatedAt     time.Time       `json:"updated_at"`
-	NextUpdateAt  time.Time       `json:"next_update_at"`
-	QuoteCurrency string          `json:"quote_currency"`
-	ChainID       int             `json:"chain_id"`
-	Items         []PortfolioItem `json:"items"`
-	Pagination    Pagination      `json:"pagination"`
+// Portfolios returns list of items for portfolio endpoint.
+type Portfolios struct {
+	Address       string      `json:"address"`
+	UpdatedAt     time.Time   `json:"updated_at"`
+	NextUpdateAt  time.Time   `json:"next_update_at"`
+	QuoteCurrency string      `json:"quote_currency"`
+	ChainID       int         `json:"chain_id"`
+	Items         []Portfolio `json:"items"`
+	Pagination    Pagination  `json:"pagination"`
 }
 
-type PortfolioItem struct {
-	ContractDecimals     int       `json:"contract_decimals"`
-	ContractName         string    `json:"contract_name"`
-	ContractTickerSymbol string    `json:"contract_ticker_symbol"`
-	ContractAddress      string    `json:"contract_address"`
-	SupportsErc          []string  `json:"supports_erc"`
-	LogoUrl              string    `json:"logo_url"`
-	Type                 string    `json:"type"`
-	Balance              string    `json:"balance"`
-	QuoteRate            float64   `json:"quote_rate"`
-	Quote                float64   `json:"quote"`
-	NftData              []NftData `json:"nft_data"`
-	Holdings             []Holding `json:"holdings"`
+// Transactions returns list of items for transaction endpoint.
+type Transactions struct {
+	Address       string        `json:"address"`
+	UpdatedAt     time.Time     `json:"updated_at"`
+	NextUpdateAt  time.Time     `json:"next_update_at"`
+	QuoteCurrency string        `json:"quote_currency"`
+	ChainId       int           `json:"chain_id"`
+	Items         []Transaction `json:"items"`
+	Pagination    Pagination    `json:"pagination"`
+}
+
+// Blocks returns list of items for block endpoint.
+type Blocks struct {
+	UpdatedAt time.Time `json:"updated_at"`
+	Items     []struct {
+		SignedAt time.Time `json:"signed_at"`
+		Height   int       `json:"height"`
+	} `json:"items"`
+	Pagination Pagination `json:"pagination"`
+}
+
+// LogEvents returns list of log items.
+type LogEvents struct {
+	UpdatedAt  time.Time  `json:"updated_at"`
+	Items      []LogEvent `json:"items"`
+	Pagination Pagination `json:"pagination"`
+}
+
+// NFTMetadata returns list of items for nft external metadata endpoint.
+type NFTMetadata struct {
+	Items      []Portfolio `json:"items"`
+	Pagination Pagination  `json:"pagination"`
+}
+
+type Portfolio struct {
+	ContractDecimals     int        `json:"contract_decimals"`
+	ContractName         string     `json:"contract_name"`
+	ContractTickerSymbol string     `json:"contract_ticker_symbol"`
+	ContractAddress      string     `json:"contract_address"`
+	SupportsErc          []string   `json:"supports_erc"`
+	LogoUrl              string     `json:"logo_url"`
+	Type                 string     `json:"type"`
+	Balance              string     `json:"balance"`
+	QuoteRate            float64    `json:"quote_rate"`
+	Quote                float64    `json:"quote"`
+	NftData              []NftData  `json:"nft_data"`
+	Holdings             []Holdings `json:"holdings"`
+}
+
+type Holdings struct {
+	Timestamp time.Time `json:"timestamp"`
+	QuoteRate float64   `json:"quote_rate"`
+	Open      Holding   `json:"open"`
+	High      Holding   `json:"high"`
+	Low       Holding   `json:"low"`
+	Close     Holding   `json:"close"`
 }
 
 type Holding struct {
-	Timestamp time.Time `json:"timestamp"`
-	QuoteRate float64   `json:"quote_rate"`
-	Open      struct {
-		Balance string  `json:"balance"`
-		Quote   float64 `json:"quote"`
-	} `json:"open"`
-	High struct {
-		Balance string  `json:"balance"`
-		Quote   float64 `json:"quote"`
-	} `json:"high"`
-	Low struct {
-		Balance string  `json:"balance"`
-		Quote   float64 `json:"quote"`
-	} `json:"low"`
-	Close struct {
-		Balance string  `json:"balance"`
-		Quote   float64 `json:"quote"`
-	} `json:"close"`
+	Balance string  `json:"balance"`
+	Quote   float64 `json:"quote"`
 }
 
-type NftData struct {
-	TokenID           string       `json:"token_id"`
-	TokenBalance      string       `json:"token_balance"`
-	TokenUrl          string       `json:"token_url"`
-	SupportsErc       []string     `json:"supports_erc"`
-	TokenPriceWei     interface{}  `json:"token_price_wei"`
-	TokenQuoteRateEth interface{}  `json:"token_quote_rate_eth"`
-	Owner             string       `json:"owner"`
-	ExternalData      ExternalData `json:"external_data"`
-}
-
-type ExternalData struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Image       string `json:"image"`
-	ExternalUrl string `json:"external_url"`
-	Attributes  []struct {
-		TraitType string      `json:"trait_type"`
-		Value     interface{} `json:"value"`
-	} `json:"attributes"`
-	Owner string `json:"owner"`
-}
-
-type TransactionData struct {
-	Address       string            `json:"address"`
-	UpdatedAt     time.Time         `json:"updated_at"`
-	NextUpdateAt  time.Time         `json:"next_update_at"`
-	QuoteCurrency string            `json:"quote_currency"`
-	ChainId       int               `json:"chain_id"`
-	Items         []TransactionItem `json:"items"`
-	Pagination    Pagination        `json:"pagination"`
-}
-
-type TransactionItem struct {
+type Transaction struct {
 	BlockSignedAt    time.Time   `json:"block_signed_at"`
 	TxHash           string      `json:"tx_hash"`
 	TxOffset         int         `json:"tx_offset"`
@@ -164,29 +162,6 @@ type TransactionItem struct {
 	GasQuote         float64     `json:"gas_quote"`
 	GasQuoteRate     float64     `json:"gas_quote_rate"`
 	LogEvents        []LogEvent  `json:"log_events"`
-}
-
-type BlockData struct {
-	UpdatedAt time.Time `json:"updated_at"`
-	Items     []struct {
-		SignedAt time.Time `json:"signed_at"`
-		Height   int       `json:"height"`
-	} `json:"items"`
-	Pagination Pagination `json:"pagination"`
-}
-
-type LogEvents struct {
-	UpdatedAt    time.Time `json:"updated_at"`
-	Data         LogEventsData
-	Error        bool        `json:"error"`
-	ErrorMessage string      `json:"error_message"`
-	ErrorCode    interface{} `json:"error_code"`
-}
-
-type LogEventsData struct {
-	UpdatedAt  time.Time  `json:"updated_at"`
-	Items      []LogEvent `json:"items"`
-	Pagination Pagination `json:"pagination"`
 }
 
 type LogEvent struct {
@@ -215,4 +190,27 @@ type LogEvent struct {
 			Value   interface{} `json:"value"`
 		} `json:"params"`
 	} `json:"decoded"`
+}
+
+type NftData struct {
+	TokenID           string          `json:"token_id"`
+	TokenBalance      string          `json:"token_balance"`
+	TokenUrl          string          `json:"token_url"`
+	SupportsErc       []string        `json:"supports_erc"`
+	TokenPriceWei     interface{}     `json:"token_price_wei"`
+	TokenQuoteRateEth interface{}     `json:"token_quote_rate_eth"`
+	Owner             string          `json:"owner"`
+	ExternalData      NFTExternalData `json:"external_data"`
+}
+
+type NFTExternalData struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+	ExternalUrl string `json:"external_url"`
+	Attributes  []struct {
+		TraitType string      `json:"trait_type"`
+		Value     interface{} `json:"value"`
+	} `json:"attributes"`
+	Owner string `json:"owner"`
 }
