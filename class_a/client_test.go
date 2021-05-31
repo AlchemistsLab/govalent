9dac81e3e6038b096d050e7252c2fce940efe9e3
zaebee <zaebuntu@gmail.com>
2021-05-31 12:59:21 +0200

    Fix test cases.

--- class_a/client_test.go
+++ class_a/client_test.go
@@ -15,22 +15,22 @@ func TestClassA_GetTokenBalances_WhenValid(t *testing.T) {
 	tests := []struct {
 		desc     string
 		response string
-		want     PortfolioData
+		want     Portfolios
 	}{
 		{
 			desc:     "WhenEmptyResponse",
 			response: `{"data": null}`,
-			want:     PortfolioData{},
+			want:     Portfolios{},
 		},
 		{
 			desc:     "WhenNoNFT",
 			response: `{"data":{"address":"0x01","updated_at":"2021-05-05T18:00:00Z","quote_currency":"USD","chain_id":56,"items":[{"contract_name":"Binance Coin","contract_ticker_symbol":"BNB","contract_address":"0xb1","type":"cryptocurrency","balance":"10","quote_rate":645,"quote":0.02}]}}`,
-			want: PortfolioData{
+			want: Portfolios{
 				Address:       "0x01",
 				UpdatedAt:     time.Date(2021, 5, 5, 18, 0, 0, 0, time.UTC),
 				QuoteCurrency: "USD",
 				ChainID:       56,
-				Items: []PortfolioItem{
+				Items: []Portfolio{
 					{
 						ContractDecimals:     0,
 						ContractName:         "Binance Coin",
